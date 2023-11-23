ce26bd18625b2700b8835a4b56f9947cc227d88e
Jackson <jackson.thounaojam@mutualmobile.com>
2023-11-23 15:49:32 +1100

    fix relative path issues

--- js/ui.js
+++ js/ui.js
@@ -1,22 +1,21 @@
-const recipes = document.querySelector('.recipes');
+const recipes = document.querySelector(".recipes");
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   // nav menu
-  const menus = document.querySelectorAll('.side-menu');
-  M.Sidenav.init(menus, {edge: 'right'});
+  const menus = document.querySelectorAll(".side-menu");
+  M.Sidenav.init(menus, {edge: "right"});
   // add recipe form
-  const forms = document.querySelectorAll('.side-form');
-  M.Sidenav.init(forms, {edge: 'left'});
+  const forms = document.querySelectorAll(".side-form");
+  M.Sidenav.init(forms, {edge: "left"});
 });
 
 // render recipe data
 const renderRecipe = (data, id) => {
-
   const html = `
     <div class="card-panel recipe white row" data-id="${id}">
-      <img src="/img/dish.png" alt="recipe thumb">
+      <img src="img/dish.png" alt="recipe thumb">
       <div class="recipe-details">
-        <div class="recipe-title">${data.name}</div>
+        <div class="recipe-title">${data.title}</div>
         <div class="recipe-ingredients">${data.ingredients}</div>
       </div>
       <div class="recipe-delete">
@@ -25,5 +24,4 @@ const renderRecipe = (data, id) => {
     </div>
   `;
   recipes.innerHTML += html;
-
 };
